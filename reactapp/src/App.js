97dc6ee639b82b3796b699835e4051eeba73cd8c
mkinty <kintymoustapha@gmail.com>
2021-01-10 19:29:20 +0100

    First commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center-column">
+
+        <div className="item-row">
+          <span>Second task</span>
+        </div>
+
+        <div className="item-row">
+          <span>Third task</span>
+        </div>
+
+        <div className="item-row">
+          <span>Fourth task</span>
+        </div>
+
+        <div className="item-row">
+          <span>Fifth task</span>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
